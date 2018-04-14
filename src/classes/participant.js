393d98cf4c9d4f499d03fd6cee060047ca814151
Mark Moniz <megglymark@gmail.com>
2018-04-13 17:39:14 -0700

    Added Participant class and test

---
+++ src/classes/participant.js
@@ -0,0 +1,84 @@
+
+/**
+ * Describes a participant from a match
+ * @property {object} raw - The raw source of the object
+ * @property {string} id - The id of the participant within the match
+ * @property {string} playerId - The playerId of the participant
+ * @property {string} shardId - The shardId of the participant within the match
+ * @property {string} actor - The actor of the participant within the match
+ * @property {number} DBNOs - The DBNOs of the participant within the match
+ * @property {number} assists - The assists of the participant within the match
+ * @property {number} boosts - The boosts of the participant within the match
+ * @property {number} damageDealt - The damageDealt of the participant within the match
+ * @property {string} deathType - The deathType of the participant within the match
+ * @property {number} headshotKills - The headshotKills of the participant within the match
+ * @property {number} heals - The heals of the participant within the match
+ * @property {number} killPlace - The killPlace of the participant within the match
+ * @property {number} killPoints - The killPoints of the participant within the match
+ * @property {number} killPointsDelta - The killPointsDelta of the participant within the match
+ * @property {number} killStreaks - The killStreaks of the participant within the match
+ * @property {number} kills - The kills of the participant within the match
+ * @property {number} lastKillPoints - The lastKillPoints of the participant within the match
+ * @property {number} lastWinPoints - The lastWinPoints of the participant within the match
+ * @property {number} longestKill - The longestKill of the participant within the match
+ * @property {number} mostDamage - The mostDamage of the participant within the match
+ * @property {string} name - The name of the participant within the match
+ * @property {number} revives - The revives of the participant within the match
+ * @property {number} rideDistance - The rideDistance of the participant within the match
+ * @property {number} roadKills - The roadKills of the participant within the match
+ * @property {number} teamKills - The teamKills of the participant within the match
+ * @property {number} timeSurvived - The timeSurvived of the participant within the match
+ * @property {number} vehicleDestroys - The vehicleDestroys of the participant within the match
+ * @property {number} walkDistance - The walkDistance of the participant within the match
+ * @property {number} weaponsAcquired - The weaponsAcquired of the participant within the match
+ * @property {number} winPlace - The winPlace of the participant within the match
+ * @property {number} winPoints - The winPoints of the participant within the match
+ * @property {number} winPointsDelta - The winPointsDelta of the participant within the match* 
+ * 
+ *
+*/
+
+class Participant {
+  /**
+   *
+   * @param {*} source - An object with particpant data attained through
+  * a Match, or otherwise provided
+    */
+  constructor(source) {
+    this.raw = source;
+    this.id = source.id;
+    this.playerId = source.attributes.stats.playerId;
+    this.shardId = source.attributes.shardId;
+    this.DBNOs = source.attributes.stats.DBNOs;
+    this.assists = source.attributes.stats.assists;
+    this.boosts = source.attributes.stats.boosts;
+    this.damageDealt = source.attributes.stats.damageDealt;
+    this.deathType = source.attributes.stats.deathType;
+    this.headshotKills = source.attributes.stats.headshotKills;
+    this.heals = source.attributes.stats.heals;
+    this.killPlace = source.attributes.stats.killPlace;
+    this.killPoints = source.attributes.stats.killPoints;
+    this.killPointsDelta = source.attributes.stats.killPointsDelta;
+    this.killStreaks = source.attributes.stats.killStreaks;
+    this.kills = source.attributes.stats.kills;
+    this.lastKillPoints = source.attributes.stats.lastKillPoints;
+    this.lastWinPoints = source.attributes.stats.lastWinPoints;
+    this.longestKill = source.attributes.stats.longestKill;
+    this.mostDamage = source.attributes.stats.mostDamage;
+    this.name = source.attributes.stats.name;
+    this.revives = source.attributes.stats.revives;
+    this.rideDistance = source.attributes.stats.rideDistance;
+    this.roadKills = source.attributes.stats.roadKills;
+    this.teamKills = source.attributes.stats.teamKills;
+    this.timeSurvived = source.attributes.stats.timeSurvived;
+    this.vehicleDestroys = source.attributes.stats.vehicleDestroys;
+    this.walkDistance = source.attributes.stats.walkDistance;
+    this.weaponsAcquired = source.attributes.stats.weaponsAcquired;
+    this.winPlace = source.attributes.stats.winPlace;
+    this.winPoints = source.attributes.stats.winPoints;
+    this.winPointsDelta = source.attributes.stats.winPointsDelta;
+  }
+}
+  
+module.exports  =  Participant;
+  
---
+++ test/participant.js
@@ -0,0 +1,424 @@
+const expect = require('chai').expect;
+const Match = require('../src/classes/match');
+const Participant = require('../src/classes/participant');
+const rawMatchData = require('./mocks/Match.json');
+
+describe('Participant', () => {
+  let match;
+  beforeEach(() => {
+    match = new Match(rawMatchData);
+  });
+  describe('#constructor', () => {
+    it('requires a raw roster object and raw included array', () => {
+      //Incorrect
+      expect(() => {new Participant();}).to.throw(Error);
+      expect(() => {new Participant({});}).to.throw(Error);
+      //Correct
+      expect(() => {new Participant(match.rosters[0].participants[0]);}).to.not.throw(Error);
+    });
+  });
+  describe('#raw', () => {
+    let participant
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.raw).to.exist;
+    });
+    it('is an object', () => {
+      expect(participant.raw).to.be.an('object');
+    });
+  });
+  describe('#id', () => {
+    let participant
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.id).to.exist;
+    });
+    it('is a GUID string', () => {
+      expect(participant.id).to.be.a('string');
+      expect(participant.id).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+    });
+  });
+  describe('#DBNOs', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.DBNOs).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.DBNOs).to.be.a('number');
+    });
+  });
+  describe('#assists', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.assists).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.assists).to.be.a('number');
+    });
+  });
+  describe('#boosts', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.boosts).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.boosts).to.be.a('number');
+    });
+  });
+  describe('#damageDealt', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.damageDealt).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.damageDealt).to.be.a('number');
+    });
+  });
+  describe('#deathType', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.deathType).to.exist;
+    });
+    it('is a string', () => {
+      expect(participant.deathType).to.be.a('string');
+    });
+  });
+  describe('#headshotKills', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.headshotKills).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.headshotKills).to.be.a('number');
+    });
+  });
+  describe('#heals', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.heals).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.heals).to.be.a('number');
+    });
+  });
+  describe('#killPlace', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.killPlace).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.killPlace).to.be.a('number');
+    });
+  });
+  describe('#killPoints', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.killPoints).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.killPoints).to.be.a('number');
+    });
+  });
+  describe('#killPointsDelta', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.killPointsDelta).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.killPointsDelta).to.be.a('number');
+    });
+  });
+  describe('#killStreaks', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.killStreaks).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.killStreaks).to.be.a('number');
+    });
+  });
+  describe('#kills', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.kills).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.kills).to.be.a('number');
+    });
+  });
+  describe('#lastKillPoints', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.lastKillPoints).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.lastKillPoints).to.be.a('number');
+    });
+  });
+  describe('#lastWinPoints', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.lastWinPoints).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.lastWinPoints).to.be.a('number');
+    });
+  });
+  describe('#longestKill', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.longestKill).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.longestKill).to.be.a('number');
+    });
+  });
+  describe('#mostDamage', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.mostDamage).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.mostDamage).to.be.a('number');
+    });
+  });
+  describe('#name', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.name).to.exist;
+    });
+    it('is a string', () => {
+      expect(participant.name).to.be.a('string');
+    });
+  });
+  describe('#playerId', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.playerId).to.exist;
+    });
+    it('is a string', () => {
+      expect(participant.playerId).to.be.a('string');
+    });
+  });
+  describe('#revives', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.revives).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.revives).to.be.a('number');
+    });
+  });
+  describe('#rideDistance', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.rideDistance).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.rideDistance).to.be.a('number');
+    });
+  });
+  describe('#roadKills', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.roadKills).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.roadKills).to.be.a('number');
+    });
+  });
+  describe('#teamKills', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.teamKills).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.teamKills).to.be.a('number');
+    });
+  });
+  describe('#timeSurvived', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.timeSurvived).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.timeSurvived).to.be.a('number');
+    });
+  });
+  describe('#vehicleDestroys', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.vehicleDestroys).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.vehicleDestroys).to.be.a('number');
+    });
+  });
+  describe('#walkDistance', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.walkDistance).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.walkDistance).to.be.a('number');
+    });
+  });
+  describe('#weaponsAcquired', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.weaponsAcquired).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.weaponsAcquired).to.be.a('number');
+    });
+  });
+  describe('#winPlace', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.winPlace).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.winPlace).to.be.a('number');
+    });
+  });
+  describe('#winPoints', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.winPoints).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.winPoints).to.be.a('number');
+    });
+  });
+  describe('#winPointsDelta', () => {
+    let participant;
+    beforeEach(() => {
+      participant = new Participant(match.rosters[0].participants[0]);
+    });
+    it('exists', () => {
+      expect(participant).to.exist;
+      expect(participant.winPointsDelta).to.exist;
+    });
+    it('is an number', () => {
+      expect(participant.winPointsDelta).to.be.a('number');
+    });
+  });
+});
